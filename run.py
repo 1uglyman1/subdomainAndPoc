62c11b24e1323c291b9cbe61692e132caf87cc58
赵高 <114843013+1uglyman1@users.noreply.github.com>
2025-06-21 17:44:20 +0800

    Add files via upload

---
+++ run.py
@@ -0,0 +1,21 @@
+import subprocess
+import os
+
+def run_dddd(domain):
+    try:
+        command = f'./dddd -t {domain} {domain}.html'
+        subprocess.run(command, shell=True, check=True)
+        print("run_dddd 扫描任务执行成功")
+    except subprocess.CalledProcessError as e:
+        print(f"执行 run_dddd 扫描任务时出错: {e}")
+        return False
+    return True
+def set_chmode():
+    os.system("chmod +x dddd")
+if __name__ == '__main__':
+    set_chmode()
+    with open('url.txt','r') as file:
+        for f in file:
+            run_dddd(f.strip())
+        file.close()
+    print("+++++++++++++ run over +++++++++++++")
